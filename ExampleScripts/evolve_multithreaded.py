08b1d5a16e8bc55e4af9ac20ecb7b0c581160051
Frank Veenstra <frankvee@uio.no>
2023-07-26 01:20:16 +0200

    Changed \\ to / . Example scripts should therefore work on macos?

--- ExampleScripts/evolve_multithreaded.py
+++ ExampleScripts/evolve_multithreaded.py
@@ -12,8 +12,8 @@ def main():
     cfg =  config.config_handler.make_config()
     cfg['visualization']['headless'] = '1'
     cfg['environment']['evaluation_steps'] = '500'
-    cfg['experiment']['executable_path'] = "EMR_Executable\\EvolvingModularRobots"
-    cfg['environment']['simulator_to_use'] = 'modular_robot' # also known as trilobyte
+    cfg['experiment']['executable_path'] = "EMR_Executable/EvolvingModularRobots"
+    cfg['environment']['simulator_to_use'] = 'modular_robot' 
     config.config_handler.print_config(cfg)
     encoding_reference, controller_reference, evaluation_function_reference = config.config_utility.get_encoding_controller_and_evaluation_from_config(cfg)
     modules_to_use = config.config_handler.modules_to_use(cfg)
--- ExampleScripts/test_emr.py
+++ ExampleScripts/test_emr.py
@@ -6,5 +6,5 @@ if __name__=="__main__":
     from emr.config import config_handler
     cfg = config_handler.make_config()
     cfg['experiment']['executable_path'] = "./EMR_Executable/EvolvingModularRobots"
-    tests.run_all_tests(cfg, headless = True)
+    tests.run_all_tests(cfg, headless = False)
     
